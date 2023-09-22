32f81d924ceb941683a922e9daa769af185bf970
joshuazg <86587114+joshuazg@users.noreply.github.com>
2023-09-22 23:25:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def compare_prog():
         cursor = db_conn.cursor()      
         cursor.execute('SELECT * FROM ProgrammeLevel')
         programme_levels = cursor.fetchall()
-        session['compare_level'] = programme_levels
+        Session['compare_level'] = programme_levels
         
         # Get the selected program level from the form
         selected_programme_level = request.form.get('programme_level') or request.args.get('programme_level')
