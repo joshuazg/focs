c4c0416cbc8f8280973ebbdf4f709576cc61e72e
joshuazg <86587114+joshuazg@users.noreply.github.com>
2023-09-22 23:16:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ customregion = "us-east-1"
 
 
 app = Flask(__name__, static_folder='assets')
+#Session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 bucket = custombucket
 region = customregion
