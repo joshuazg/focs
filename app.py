56d0fbd4d62ff71ad91db8c4df107f512696fef8
joshuazg <86587114+joshuazg@users.noreply.github.com>
2023-09-22 23:16:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ customregion = "us-east-1"
 
 
 app = Flask(__name__, static_folder='assets')
-#Session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
